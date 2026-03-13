5643b6e9601be7b3a89fab2be7003eaae8ff9b8a
munnaMia <mdmunnamia987@gmail.com>
2026-03-13 14:56:49 +0600

    add: font cdns, and style file links

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,8 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>octaProfile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico" type="image/x-icon">
+    <title>octaProfile</title>
 </head>
 
 <body class="h-full m-0 p-0 box-border font-primary">
