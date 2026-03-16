9982aa939a7a8482560542402a95b31df305d410
munnaMia <mdmunnamia987@gmail.com>
2026-03-16 13:54:26 +0600

    reft: minor checks and rewrite

---
+++ controllers/index.php
@@ -0,0 +1,3 @@
+<?php
+
+require ""; // letter work here
--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
 <?php
 declare(strict_types=1);
 
+use Core\Util;
+
 const BASE_PATH = __DIR__ . '/../';
 
 require_once BASE_PATH . 'Core/Util.php'; // temporary...
+
+Util::setBasePath(BASE_PATH);
 
 require(BASE_PATH . 'views/index.view.php');
