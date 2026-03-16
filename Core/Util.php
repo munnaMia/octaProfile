f7c42e5fd153e562c09a7e2424c55bedb23ef2de
munnaMia <mdmunnamia987@gmail.com>
2026-03-16 13:53:36 +0600

    add: Base_path set&get method

--- Core/Util.php
+++ Core/Util.php
@@ -4,6 +4,18 @@ namespace Core;
 
 class Util
 {
+    private static $BASEPATH;
+
+    public static function setBasePath(string $str): void
+    {
+        self::$BASEPATH = $str;
+    }
+
+    public static function getBasePath(): string
+    {
+        return self::$BASEPATH ?? throw new \Exception("Basepath not initiallize.");
+    }
+
     //takes and data as an parameter. print the data on screen and kill the screen
     public static function dumpAndDie($data)
     {
