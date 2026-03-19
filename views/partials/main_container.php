68e5d22f7ff2c3354f1c50e2708f134e065afb11
munnaMia <mdmunnamia987@gmail.com>
2026-03-19 10:54:51 +0600

    refc: ui smoothing

--- views/partials/main_container.php
+++ views/partials/main_container.php
@@ -3,10 +3,11 @@
 use Core\Util;
 
 ?>
-<main class="col-span-6 w-full bg-transparent">
-    <div class="bg-white w-full h-full flex flex-col px-2 py-2.5">
+<main class="col-span-6 w-full bg-transparent h-full">
+    <div class="bg-white w-full  flex flex-col px-2 py-2.5 overflow-y-auto scroll-smooth h-full">
         <!-- form to upload a post -->
         <?php Util::require('views/partials/post_form.php') ?> 
+
         <?php Util::require('views/partials/post.php') ?>
         <?php Util::require('views/partials/post.php') ?>
         <?php Util::require('views/partials/post.php') ?>
