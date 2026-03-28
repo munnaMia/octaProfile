6226b6424e1375b14cd6a4c97c937a3df6bbff18
munnaMia <mdmunnamia987@gmail.com>
2026-03-28 15:35:53 +0600

    reft: db connect & configs format

--- Core/Config.php
+++ Core/Config.php
@@ -14,17 +14,19 @@ class Config
         $dotenv->load();
 
         self::$configs = [
-            "dbConfig" => [
-                "host" => $_ENV["DB_HOST"],
-                "port" => $_ENV["DB_PORT"],
-                "name" => $_ENV["DB_NAME"],
-                "charset" => $_ENV["CHARSET"],
-            ],
-            "db_user" => [
-                "db" => $_ENV["DB"],
-                "user" => $_ENV["DB_USER"],
-                "password" => $_ENV["DB_PASSWORD"],
-            ],
+            "database" => [
+                "db_configs" => [
+                    "host" => $_ENV["DB_HOST"],
+                    "dbname" => $_ENV["DB_NAME"],
+                    "port" => $_ENV["DB_PORT"],
+                    "charset" => $_ENV["CHARSET"],
+                ],
+                "db_user" => [
+                    "db" => $_ENV["DB"],
+                    "user" => $_ENV["DB_USER"],
+                    "password" => $_ENV["DB_PASSWORD"],
+                ],
+            ]
         ];
     }
 
@@ -34,6 +36,6 @@ class Config
             self::loadConfig();
         }
 
-        return self::$configs;
+        return self::$configs[$key];
     }
 }
--- Core/Database.php
+++ Core/Database.php
@@ -13,6 +13,7 @@ CHARSET="utf8mb4"
 namespace Core;
 
 use PDO;
+use PDOException;
 
 class Database
 {
@@ -20,8 +21,18 @@ class Database
 
     public function __construct($configs)
     {
-        $dsn = "{$configs['db_user']['db']}:" . http_build_query($configs['dbConfig'], "", ";");
+        $dsn = "{$configs['db_user']['db']}:" . http_build_query($configs['db_configs'], "", ";");
 
-        $this->connection = new PDO($dsn, $configs['db_user']['user'], $configs['db_user']['password']);
+        $this->connection = new PDO(
+            $dsn,
+            $configs['db_user']['user'],
+            $configs['db_user']['password'],
+            [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
+
+        // $stmt = $this->connection->query("SELECT DATABASE();");
+        // Util::dumpAndDie($stmt->fetch());
     }
 }
